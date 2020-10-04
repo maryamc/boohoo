a1751a8b9fb40f2ec175c6b62ab6904151635015
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-04 19:44:10 -0400

    added first function to generate password prompt

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # boohoo
+This is the password generator homework assignment.
+
+In this assignment the password must be between 8-128 characters which encompasses numerical values, special characters, uppercase, and lowercase letters
+
+For the fist function "generatePassword();", the user will be prompted a question about how many characters they want their password to contain.
+    - The response must be a numerical value between 8-128
+    - If the user fails to give a numerical value between 8-128, the user will be alerted that the password is either going to be too short or too long, respectively
+
+
--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-// variables
+// variables to be used in order to generate password
+// decided to only use a few special characters from keyboard
+var specialCharacters = ["~", "$", "@", "&", "%", "!"]
 
-var specialCharacters = ["~","$","@","&"]
+var numericalValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-var numericalValues = ["1","2","3","4","5","6","7","8","9","0"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var lowerCaseLetters = 
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//create a function to prompt user
-function getPasswordOptions() {
-  var length = parseInt(
-    prompt("How many characters would you like your password to contain?")
-  );
-}
+//create a function to prompt user how many characters they would like their password to contain
+function generatePassword() {
+  var length = parseInt(prompt("How many characters would you like your password to contain?"));
 
-if (isNaN(length) === true){
-  alert("password length must be provided with a number");
-  return;
-}
+  // conditional to check pass length
+  if (length < 8) {
+    alert("password must be at least 8 characters");
+    return;
+  }
+  if (length > 128) {
+    alert("password must be less than 128 characters");
+    return;
+  }
 
-//conditional to check pass length
+// // ask what characters and add conditionals
 
-if (length < 8){
-  alert("password must be at least 8 characters");
-  return;
-}
-if (length > 128){
-  alert("password must be less than 128 characters");
-  return;
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// ask what characters and add conditionals
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//   passwordText.value = password;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// }
 
-  passwordText.value = password;
+// // Add event listener to generate button
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
