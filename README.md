a177285270736bc582cf028855fc7a676af995e4
Maryam Chaudhry <maryamc1798@gmail.com>
2020-10-04 21:58:47 -0400

    final commit for now until its graded, not working properly

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ This is the password generator homework assignment.
 
 In this assignment the password must be between 8-128 characters which encompasses numerical values, special characters, uppercase, and lowercase letters
 
-For the fist function "generatePassword();", the user will be prompted a question about how many characters they want their password to contain.
+For the first function "generatePassword();", the user will be prompted a question about how many characters they want their password to contain.
     - The response must be a numerical value between 8-128
     - If the user fails to give a numerical value between 8-128, the user will be alerted that the password is either going to be too short or too long, respectively
+    - this also encompasses the different character values that will be asked from the user
+
+I have everything there, however I cant seem to piece it all together. 
+
+All the prompts are working, however my generator keeps coming up as undefined and I honestly do not know how to fix it at this point, so this is my submission for now. Will most definitely be re-doing this assignment. 
 
 
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // variables to be used in order to generate password
 // decided to only use a few special characters from keyboard
-var totalCharacters = [];
 
-var specialCharacters = ["~", "$", "@", "&", "%", "!"]
+var totalCharacters = [];
+console.log(totalCharacters);
+
+var specialCharacters = ["~", "$", "@", "&", "%", "!"];
 
 var numericalValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
@@ -10,18 +12,21 @@ var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var arr = [specialCharacters, numericalValues, lowerCaseLetters, upperCaseLetters];
+
+
+
 //create a function to prompt user how many characters they would like their password to contain
-// also includes selectedcharacters function to prompt character selection
+
 
 function generatePassword() {
-
-  selectedCharacters();
 
   var length = parseInt(prompt("How many characters would you like your password to contain?"));
 
   // conditionals to check password length and make sure it generates
   if (isNaN(length) === true) {
     alert("password must be given with a number");
+    return;
   }
 
   if (length < 8) {
@@ -32,48 +37,68 @@ function generatePassword() {
     alert("password must be less than 128 characters");
     return;
   }
-}
+
+
 
   // creating variables to ask user what different types of characters they would like to include in their password
+  // tried using .join() like others in class said, however I cant get this to work out
 
-  function selectedCharacters(){
-
-  var withSpecialCharacter = confirm("Do you want your password to include special characters?");
-  var withNumericalValue = confirm("Do you want your password to include numerical values?");
-  var withLowerCase = confirm("Do you want your password to include lowercase letters?");
-  var withUpperCase = confirm("Do you want your password to include uppercase letters?")
-
-if (withSpecialCharacter){
-  totalCharacters = totalCharacters.concat(specialCharacters);
-}
-if (withNumericalValue) {
-  totalCharacters = totalCharacters.concat(withNumericalValue);
-}
-if (withLowerCase) {
-  totalCharacters = totalCharacters.concat(withLowerCase);
-}
-if (withUpperCase) {
-  totalCharacters = totalCharacters.concant(withUpperCase);
-}
-  }
-
-
-
-
-
-  // Assignment Code
-
-  var generateBtn = document.querySelector("#generate");
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
+  var _SpecialCharacter = confirm("Click OK to use special characters");
+  if (_SpecialCharacter) {
+    specialCharacters.join();
+    totalCharacters.push(specialCharacters);
+    console.log(_SpecialCharacter);
 
   }
 
-  // Add event listener to generate button
-  
-  generateBtn.addEventListener("click", writePassword);
+
+  var _NumericalValue = confirm("Click OK to use numerical values");
+  if (_NumericalValue) {
+    numericalValues.join();
+    totalCharacters.push(numericalValues);
+    console.log(_NumericalValue);
+
+  }
+
+
+  var _LowerCase = confirm("Click OK to use lowercase letters");
+  if (_LowerCase) {
+    lowerCaseLetters.join();
+    totalCharacters.push(lowerCaseLetters);
+    console.log(_LowerCase);
+
+  }
+
+
+  var _UpperCase = confirm("Click OK to use uppercase letters");
+  if (_UpperCase) {
+    upperCaseLetters.join();
+    totalCharacters.push(upperCaseLetters)
+    console.log(_UpperCase);
+
+  }
+
+  // this is supposed to concatonate the string but its not working, because I still cant figure out not how to link or define variables for this part 
+  for (var i = 0; i < length; ++i) {
+    arr[Math.floor(Math.random() * arr.length)];
+  }
+
+
+}
+
+// Assignment Code
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
